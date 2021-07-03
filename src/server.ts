356afd23a310d0c795965c478e413326c376cfa8
Romulo S. Ramos <rsromulo@msn.com>
2021-07-03 16:52:41 -0300

    Add Swagger

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,18 @@ import dotenv from 'dotenv';
 import "reflect-metadata";
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
+import swaggerUI from "swagger-ui-express"
 import cors from "cors";
 
 import { router } from "./routes";
+import swaggerDocs from "./docs/swagger.json";
 import "./database";
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(router);
+app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerDocs))
+app.use('/v1',router);
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
   if (err instanceof Error) {
     return response.status(400).json({
