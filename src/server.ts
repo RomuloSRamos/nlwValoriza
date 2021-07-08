5e54fab4642bbbf6dbe17196bf32eba5c5638a3e
Romulo S. Ramos <rsromulo@msn.com>
2021-07-08 17:14:16 -0300

    ADD more docs

--- src/server.ts
+++ src/server.ts
@@ -12,8 +12,8 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerDocs))
 app.use('/v1',router);
+app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerDocs))
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
   if (err instanceof Error) {
     return response.status(400).json({
